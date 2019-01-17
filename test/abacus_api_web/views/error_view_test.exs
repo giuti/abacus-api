8e1efa3e12a606d81be2402d5f82f753118b23a7
Sergio <s.valle.serrano@gmail.com>
2019-01-17 13:36:05 +0100

    Fix error test

--- test/abacus_api_web/views/error_view_test.exs
+++ test/abacus_api_web/views/error_view_test.exs
@@ -5,11 +5,11 @@ defmodule AbacusApiWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(AbacusApiWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(AbacusApiWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Endpoint not found!"}}
   end
 
   test "renders 500.json" do
     assert render(AbacusApiWeb.ErrorView, "500.json", []) ==
-             %{errors: %{detail: "Internal Server Error"}}
+             %{errors: %{detail: "D'oh! Internal server error."}}
   end
 end
