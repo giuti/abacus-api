5b07b64d5cbd908b4bfa37eea550e17c6639131e
Sergio <s.valle.serrano@gmail.com>
2019-02-01 12:52:21 +0100

    Update prod config

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,6 @@
 use Mix.Config
 
 config :abacus_api, AbacusApiWeb.Endpoint,
-  load_from_system_env: true,
   url: [scheme: "https", host: "abacus-api.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
@@ -11,7 +10,6 @@ config :logger, level: :info
 
 # Configure your database
 config :abacus_api, AbacusApi.Repo,
-  adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
